78bfa67474a2ab0a5e51bcb6043967ac3dcd09be
TriQue06 <baristanlik@gmail.com>
2024-04-15 21:04:32 +0300

    .

--- ideologies.js
+++ ideologies.js
@@ -7,5 +7,464 @@ ideologies = [
             "democrat_authoritarian": 100,
             "progress_tradition": 90
         }
+    },
+    {
+        "name": "Liberteryen Komünizm",
+        "stats": {
+            "socialist_capitalist": 100,
+            "globalist_isolationist": 70,
+            "democrat_authoritarian": 80,
+            "progress_tradition": 80
+        }
+    },
+    {
+        "name": "Troçkizm",
+        "stats": {
+            "socialist_capitalist": 100,
+            "globalist_isolationist": 100,
+            "democrat_authoritarian": 60,
+            "progress_tradition": 80
+        }
+    },
+    {
+        "name": "Marksizm",
+        "stats": {
+            "socialist_capitalist": 100,
+            "globalist_isolationist": 70,
+            "democrat_authoritarian": 40,
+            "progress_tradition": 80
+        }
+    },
+    {
+        "name": "De Leonism",
+        "stats": {
+            "socialist_capitalist": 100,
+            "globalist_isolationist": 30,
+            "democrat_authoritarian": 30,
+            "progress_tradition": 80
+        }
+    },
+    {
+        "name": "Leninism",
+        "stats": {
+            "socialist_capitalist": 100,
+            "globalist_isolationist": 40,
+            "democrat_authoritarian": 20,
+            "progress_tradition": 70
+        }
+    },
+    {
+        "name": "Stalinism/Maoism",
+        "stats": {
+            "socialist_capitalist": 100,
+            "globalist_isolationist": 20,
+            "democrat_authoritarian": 0,
+            "progress_tradition": 60
+        }
+    },
+    {
+        "name": "Religious Communism",
+        "stats": {
+            "socialist_capitalist": 100,
+            "globalist_isolationist": 50,
+            "democrat_authoritarian": 30,
+            "progress_tradition": 30
+        }
+    },
+    {
+        "name": "State Socialism",
+        "stats": {
+            "socialist_capitalist": 80,
+            "globalist_isolationist": 30,
+            "democrat_authoritarian": 30,
+            "progress_tradition": 70
+        }
+    },
+    {
+        "name": "Theocratic Socialism",
+        "stats": {
+            "socialist_capitalist": 80,
+            "globalist_isolationist": 50,
+            "democrat_authoritarian": 30,
+            "progress_tradition": 20
+        }
+    },
+    {
+        "name": "Religious Socialism",
+        "stats": {
+            "socialist_capitalist": 80,
+            "globalist_isolationist": 50,
+            "democrat_authoritarian": 70,
+            "progress_tradition": 20
+        }
+    },
+    {
+        "name": "Democratic Socialism",
+        "stats": {
+            "socialist_capitalist": 80,
+            "globalist_isolationist": 50,
+            "democrat_authoritarian": 50,
+            "progress_tradition": 80
+        }
+    },
+    {
+        "name": "Revolutionary Socialism",
+        "stats": {
+            "socialist_capitalist": 80,
+            "globalist_isolationist": 20,
+            "democrat_authoritarian": 50,
+            "progress_tradition": 70
+        }
+    },
+    {
+        "name": "Libertarian Socialism",
+        "stats": {
+            "socialist_capitalist": 80,
+            "globalist_isolationist": 80,
+            "democrat_authoritarian": 80,
+            "progress_tradition": 80
+        }
+    },
+    {
+        "name": "Anarcho-Syndicalism",
+        "stats": {
+            "socialist_capitalist": 80,
+            "globalist_isolationist": 50,
+            "democrat_authoritarian": 100,
+            "progress_tradition": 80
+        }
+    },
+    {
+        "name": "Left-Wing Populism",
+        "stats": {
+            "socialist_capitalist": 60,
+            "globalist_isolationist": 40,
+            "democrat_authoritarian": 30,
+            "progress_tradition": 70
+        }
+    },
+    {
+        "name": "Theocratic Distributism",
+        "stats": {
+            "socialist_capitalist": 60,
+            "globalist_isolationist": 40,
+            "democrat_authoritarian": 30,
+            "progress_tradition": 20
+        }
+    },
+    {
+        "name": "Distributism",
+        "stats": {
+            "socialist_capitalist": 60,
+            "globalist_isolationist": 50,
+            "democrat_authoritarian": 50,
+            "progress_tradition": 20
+        }
+    },
+    {
+        "name": "Social Liberalism",
+        "stats": {
+            "socialist_capitalist": 60,
+            "globalist_isolationist": 60,
+            "democrat_authoritarian": 60,
+            "progress_tradition": 80
+        }
+    },
+    {
+        "name": "Christian Democracy",
+        "stats": {
+            "socialist_capitalist": 60,
+            "globalist_isolationist": 60,
+            "democrat_authoritarian": 50,
+            "progress_tradition": 30
+        }
+    },
+    {
+        "name": "Social Democracy",
+        "stats": {
+            "socialist_capitalist": 60,
+            "globalist_isolationist": 70,
+            "democrat_authoritarian": 60,
+            "progress_tradition": 80
+        }
+    },
+    {
+        "name": "Progressivism",
+        "stats": {
+            "socialist_capitalist": 60,
+            "globalist_isolationist": 80,
+            "democrat_authoritarian": 60,
+            "progress_tradition": 100
+        }
+    },
+    {
+        "name": "Anarcho-Mutualism",
+        "stats": {
+            "socialist_capitalist": 60,
+            "globalist_isolationist": 50,
+            "democrat_authoritarian": 100,
+            "progress_tradition": 70
+        }
+    },
+    {
+        "name": "National Totalitarianism",
+        "stats": {
+            "socialist_capitalist": 50,
+            "globalist_isolationist": 20,
+            "democrat_authoritarian": 0,
+            "progress_tradition": 50
+        }
+    },
+    {
+        "name": "Global Totalitarianism",
+        "stats": {
+            "socialist_capitalist": 50,
+            "globalist_isolationist": 80,
+            "democrat_authoritarian": 0,
+            "progress_tradition": 50
+        }
+    },
+    {
+        "name": "Technocracy",
+        "stats": {
+            "socialist_capitalist": 60,
+            "globalist_isolationist": 60,
+            "democrat_authoritarian": 20,
+            "progress_tradition": 70
+        }
+    },
+    {
+        "name": "Centrist",
+        "stats": {
+            "socialist_capitalist": 50,
+            "globalist_isolationist": 50,
+            "democrat_authoritarian": 50,
+            "progress_tradition": 50
+        }
+    },
+    {
+        "name": "Liberalism",
+        "stats": {
+            "socialist_capitalist": 50,
+            "globalist_isolationist": 60,
+            "democrat_authoritarian": 60,
+            "progress_tradition": 60
+        }
+    },
+    {
+        "name": "Religious Anarchism",
+        "stats": {
+            "socialist_capitalist": 50,
+            "globalist_isolationist": 50,
+            "democrat_authoritarian": 100,
+            "progress_tradition": 20
+        }
+    },
+    {
+        "name": "Right-Wing Populism",
+        "stats": {
+            "socialist_capitalist": 40,
+            "globalist_isolationist": 30,
+            "democrat_authoritarian": 30,
+            "progress_tradition": 30
+        }
+    },
+    {
+        "name": "Moderate Conservatism",
+        "stats": {
+            "socialist_capitalist": 40,
+            "globalist_isolationist": 40,
+            "democrat_authoritarian": 50,
+            "progress_tradition": 30
+        }
+    },
+    {
+        "name": "Reactionary",
+        "stats": {
+            "socialist_capitalist": 40,
+            "globalist_isolationist": 40,
+            "democrat_authoritarian": 40,
+            "progress_tradition": 10
+        }
+    },
+    {
+        "name": "Social Libertarianism",
+        "stats": {
+            "socialist_capitalist": 60,
+            "globalist_isolationist": 70,
+            "democrat_authoritarian": 80,
+            "progress_tradition": 70
+        }
+    },
+    {
+        "name": "Libertarianism",
+        "stats": {
+            "socialist_capitalist": 40,
+            "globalist_isolationist": 60,
+            "democrat_authoritarian": 80,
+            "progress_tradition": 60
+        }
+    },
+    {
+        "name": "Anarcho-Egoism",
+        "stats": {
+            "socialist_capitalist": 40,
+            "globalist_isolationist": 50,
+            "democrat_authoritarian": 100,
+            "progress_tradition": 50
+        }
+    },
+    {
+        "name": "Nazism",
+        "stats": {
+            "socialist_capitalist": 40,
+            "globalist_isolationist": 0,
+            "democrat_authoritarian": 0,
+            "progress_tradition": 5
+        }
+    },
+    {
+        "name": "Autocracy",
+        "stats": {
+            "socialist_capitalist": 50,
+            "globalist_isolationist": 20,
+            "democrat_authoritarian": 20,
+            "progress_tradition": 50
+        }
+    },
+    {
+        "name": "Fascism",
+        "stats": {
+            "socialist_capitalist": 40,
+            "globalist_isolationist": 20,
+            "democrat_authoritarian": 20,
+            "progress_tradition": 20
+        }
+    },
+    {
+        "name": "Capitalist Fascism",
+        "stats": {
+            "socialist_capitalist": 20,
+            "globalist_isolationist": 20,
+            "democrat_authoritarian": 20,
+            "progress_tradition": 20
+        }
+    },
+    {
+        "name": "Conservatism",
+        "stats": {
+            "socialist_capitalist": 30,
+            "globalist_isolationist": 40,
+            "democrat_authoritarian": 40,
+            "progress_tradition": 20
+        }
+    },
+    {
+        "name": "Neo-Liberalism",
+        "stats": {
+            "socialist_capitalist": 30,
+            "globalist_isolationist": 30,
+            "democrat_authoritarian": 50,
+            "progress_tradition": 60
+        }
+    },
+    {
+        "name": "Classical Liberalism",
+        "stats": {
+            "socialist_capitalist": 30,
+            "globalist_isolationist": 60,
+            "democrat_authoritarian": 60,
+            "progress_tradition": 80
+        }
+    },
+    {
+        "name": "Authoritarian Capitalism",
+        "stats": {
+            "socialist_capitalist": 20,
+            "globalist_isolationist": 30,
+            "democrat_authoritarian": 20,
+            "progress_tradition": 40
+        }
+    },
+    {
+        "name": "State Capitalism",
+        "stats": {
+            "socialist_capitalist": 20,
+            "globalist_isolationist": 50,
+            "democrat_authoritarian": 30,
+            "progress_tradition": 50
+        }
+    },
+    {
+        "name": "Neo-Conservatism",
+        "stats": {
+            "socialist_capitalist": 20,
+            "globalist_isolationist": 20,
+            "democrat_authoritarian": 40,
+            "progress_tradition": 20
+        }
+    },
+    {
+        "name": "Fundamentalism",
+        "stats": {
+            "socialist_capitalist": 20,
+            "globalist_isolationist": 30,
+            "democrat_authoritarian": 30,
+            "progress_tradition": 5
+        }
+    },
+    {
+        "name": "Libertarian Capitalism",
+        "stats": {
+            "socialist_capitalist": 20,
+            "globalist_isolationist": 50,
+            "democrat_authoritarian": 80,
+            "progress_tradition": 60
+        }
+    },
+    {
+        "name": "Market Anarchism",
+        "stats": {
+            "socialist_capitalist": 20,
+            "globalist_isolationist": 50,
+            "democrat_authoritarian": 100,
+            "progress_tradition": 50
+        }
+    },
+    {
+        "name": "Objectivism",
+        "stats": {
+            "socialist_capitalist": 10,
+            "globalist_isolationist": 50,
+            "democrat_authoritarian": 90,
+            "progress_tradition": 40
+        }
+    },
+    {
+        "name": "Totalitarian Capitalism",
+        "stats": {
+            "socialist_capitalist": 0,
+            "globalist_isolationist": 30,
+            "democrat_authoritarian": 0,
+            "progress_tradition": 50
+        }
+    },
+    {
+        "name": "Ultra-Capitalism",
+        "stats": {
+            "socialist_capitalist": 0,
+            "globalist_isolationist": 40,
+            "democrat_authoritarian": 50,
+            "progress_tradition": 50
+        }
+    },
+    {
+        "name": "Anarcho-Capitalism",
+        "stats": {
+            "socialist_capitalist": 0,
+            "globalist_isolationist": 50,
+            "democrat_authoritarian": 100,
+            "progress_tradition": 50
+        }
     }
 ];
