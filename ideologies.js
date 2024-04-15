b789bd30b6f6eba190895f628664428a3e571d0f
TriQue06 <baristanlik@gmail.com>
2024-04-15 22:50:30 +0300

    Update ideologies.js

--- ideologies.js
+++ ideologies.js
@@ -31,21 +31,91 @@ ideologies = [
         "name": "Demokratik Sosyalizm",
         "stats": {
             "socialist_capitalist": 75,
-            "security_freedom": 75
+            "security_freedom": 25
         }
     },
     {
         "name": "Sosyal Demokrasi",
         "stats": {
-            "socialist_capitalist": 60,
-            "security_freedom": 60
+            "socialist_capitalist": 58,
+            "security_freedom": 58
         }
     },
     {
         "name": "İlerlemecilik / Yeşil Siyaset",
         "stats": {
-            "socialist_capitalist": 60,
-            "security_freedom": 40
+            "socialist_capitalist": 58,
+            "security_freedom": 42
+        }
+    },
+    {
+        "name": "Marksizm",
+        "stats": {
+            "socialist_capitalist": 100,
+            "security_freedom": 50
+        }
+    },
+    {
+        "name": "Liberal Sosyalizm",
+        "stats": {
+            "socialist_capitalist": 58,
+            "security_freedom": 75
+        }
+    },
+    {
+        "name": "Anarşizm",
+        "stats": {
+            "socialist_capitalist": 50,
+            "security_freedom": 100
+        }
+    },
+    {
+        "name": "Sosyalizm",
+        "stats": {
+            "socialist_capitalist": 75,
+            "security_freedom": 75
+        }
+    },
+    {
+        "name": "Muhafazakârlık ve Milliyetçilik",
+        "stats": {
+            "socialist_capitalist": 25,
+            "security_freedom": 75
+        }
+    },
+    {
+        "name": "Faşizm",
+        "stats": {
+            "socialist_capitalist": 33,
+            "security_freedom": 100
+        }
+    },
+    {
+        "name": "Kapitalizm",
+        "stats": {
+            "socialist_capitalist": 0,
+            "security_freedom": 50
+        }
+    },
+    {
+        "name": "Liberalizm",
+        "stats": {
+            "socialist_capitalist": 33,
+            "security_freedom": 50
+        }
+    },
+    {
+        "name": "Liberteryenizm",
+        "stats": {
+            "socialist_capitalist": 33,
+            "security_freedom": 75
+        }
+    },
+    {
+        "name": "Liberteryenizm",
+        "stats": {
+            "socialist_capitalist": 33,
+            "security_freedom": 75
         }
     }
 ];
